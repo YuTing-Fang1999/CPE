d3000ead89e596669dbfdcbd9e2d34f247f2bb03
YuTing-Fang1999 <66452317+YuTing-Fang1999@users.noreply.github.com>
2021-12-15 17:08:57 +0800

    Add files via upload

---
+++ uva10041.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	while(n--){
+		int r;
+		cin>>r;
+		
+		int s[r];
+		for(int i=0;i<r;++i){
+			cin>>s[i];
+		}
+		sort(&s[0],&s[r]);
+		
+		int mid = s[r/2];
+		int distances = 0;
+		for(int i=0;i<r;++i){
+			distances+=abs(s[i]-mid);
+		}
+		
+		printf("%d\n",distances);
+		
+	}
+	return 0;
+}
