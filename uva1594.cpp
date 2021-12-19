799761a5dd1b08af71b97fa0c109b00bf1d37f02
YuTing-Fang1999 <66452317+YuTing-Fang1999@users.noreply.github.com>
2021-12-19 09:54:06 +0800

    Update uva1594.cpp

--- uva1594.cpp
+++ uva1594.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
-#include <set>
-#include <string>
+
 using namespace std;
 void Ducci(int arr[],int n2){
 	int a0=arr[0];
@@ -9,15 +8,6 @@ void Ducci(int arr[],int n2){
 		arr[i]=abs(arr[i]-arr[i+1]);
 	}
 	arr[n2-1]=abs(arr[n2-1]-a0);
-}
-
-string to_string(int arr[],int n2){
-	string str;
-	for(int i=0;i<n2;++i){
-		str.push_back(arr[i] + '0');
-		str.push_back(',');
-	}
-	return str;
 }
 
 bool isZero(int arr[],int n2){
@@ -29,28 +19,26 @@ bool isZero(int arr[],int n2){
 
 int main(){
 	int n; cin>>n;
+
 	while(n--){
 		int n2; cin>>n2;
-		int arr[1005];
-		for(int i=0;i<n2;++i){
+		int arr[20];
+		for(int i=0;i<n2;++i)
 			cin>>arr[i];
-		}
 		
-		set<string> mySet;
-		while(1){
+		bool isLoop = true;
+		//自作聰明，將陣列變成字串存入set反而比較慢
+		//直接爆開比較快==
+		for(int i=0;i<1001;++i){
 			Ducci(arr,n2);
 			if(isZero(arr,n2)) {
+				isLoop = false;
 				cout<<"ZERO"<<endl;
 				break;
 			}
-			
-			string str = to_string(arr,n2);
-			//cout<<str<<endl;
-			if(mySet.find(str)!=mySet.end()){
-				cout<<"LOOP"<<endl;
-				break;
-			}
-			mySet.insert(str);
+		}
+		if(isLoop){
+			cout<<"LOOP"<<endl;
 		}
 	}
 	return 0;
