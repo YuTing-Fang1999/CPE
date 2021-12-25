3679e49a2268f837be4301c93ad71a227f10bd24
YuTing-Fang1999 <66452317+YuTing-Fang1999@users.noreply.github.com>
2021-12-25 19:43:20 +0800

    Add files via upload

--- UVA406/uva406.cpp
+++ UVA406/uva406.cpp
@@ -6,11 +6,13 @@ vector<int> PRIME;
 int len[1001] = {0};
 int main(){
 	int N,C;
+	//init
 	PRIME.push_back(1);
 	PRIME.push_back(2);
 	len[1] = 1; len[2] = 2;
 	while(cin>>N>>C){
 		printf("%d %d:",N,C);
+		//fing prime
 		for(int i=PRIME.back()+1;i<=N;++i){
 			bool flag = true;
 			for(int j=1;j<PRIME.size();++j){
@@ -22,11 +24,13 @@ int main(){
 			if(flag) PRIME.push_back(i);
 			len[i] = PRIME.size();
 		}
+		//fing boundary
 		--C;
 		int a = (len[N])/2-C-(len[N]%2==0);
 		if(a<0) a=0;
 		int b = (len[N])/2+C;
 		if(b>=len[N]) b=len[N]-1;
+		//output
 		for(int i=a; i<=b; ++i){
 			cout<<" "<<PRIME[i];
 		}
