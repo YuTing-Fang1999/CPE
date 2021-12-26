97e133339507245fcd218e33ea61b02156be09ce
YuTing-Fang1999 <66452317+YuTing-Fang1999@users.noreply.github.com>
2021-12-26 09:01:58 +0800

    Update uva406.cpp

--- UVA406/uva406.cpp
+++ UVA406/uva406.cpp
@@ -3,17 +3,19 @@
 using namespace std;
 
 vector<int> PRIME;
-int len[1001] = {0};
+vector<int> LEN;
 int main(){
 	int N,C;
 	//init
 	PRIME.push_back(1);
 	PRIME.push_back(2);
-	len[1] = 1; len[2] = 2;
+	LEN.push_back(0);
+	LEN.push_back(1);
+	LEN.push_back(2);
 	while(cin>>N>>C){
 		printf("%d %d:",N,C);
 		//fing prime
-		for(int i=PRIME.back()+1;i<=N;++i){
+		for(int i=LEN.size();i<=N;++i){
 			bool isPrime = true;
 			for(int j=1;j<PRIME.size();++j){
 				if(i%PRIME[j] == 0) {
@@ -22,14 +24,14 @@ int main(){
 				}
 			}
 			if(isPrime) PRIME.push_back(i);
-			len[i] = PRIME.size();
+			LEN.push_back(PRIME.size());
 		}
 		//fing boundary
 		--C;
-		int a = (len[N])/2-C-(len[N]%2==0);
+		int a = (LEN[N])/2-C-(LEN[N]%2==0);
 		if(a<0) a=0;
-		int b = (len[N])/2+C;
-		if(b>=len[N]) b=len[N]-1;
+		int b = (LEN[N])/2+C;
+		if(b>=LEN[N]) b=LEN[N]-1;
 		//output
 		for(int i=a; i<=b; ++i){
 			cout<<" "<<PRIME[i];
