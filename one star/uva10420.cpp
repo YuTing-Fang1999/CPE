10d00c570cbd602c9217142206c31ea381ad377b
YuTing-Fang1999 <66452317+YuTing-Fang1999@users.noreply.github.com>
2022-01-25 13:51:50 +0800

    Add files via upload

---
+++ one star/uva10420.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+map< string, int > myLove;
+map< string, int >::iterator iter;
+int N;
+string str;
+int main(){
+	cin>>N;
+	while(N--){
+		cin>>str;
+		cin.ignore(1024,'\n');
+		myLove[str]++;
+	}
+	for(iter=myLove.begin();iter!=myLove.end();++iter){
+		printf("%s %d\n",iter->first.c_str(), iter->second);
+	}
+	return 0;
+} 
