072510b8f470af16795c6b154c9c2eae7af250d3
YuTing-Fang1999 <66452317+YuTing-Fang1999@users.noreply.github.com>
2022-02-13 15:16:11 +0800

    Update and rename uva10530.cpp to uva10530 - Guessing Game.cpp

---
+++ uva10530 - Guessing Game.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	int N,L=1,H=10;
+	string str;
+	while(cin>>N && N){
+		getchar();//del '\n'
+		getline(cin,str);
+		if(str=="too high" && N<H){//注意N要比目前的更低才要更新
+			H=N-1;
+		}
+		else if(str=="too low" && N>L){
+			L=N+1;
+		}
+		else if(str=="right on"){
+			if(L<=N && N<=H) cout<<"Stan may be honest"<<endl;
+			else cout<<"Stan is dishonest"<<endl;
+			L=1,H=10;
+		}
+	}
+	return 0;
+}
