e4bf3c2ebaafdff87f26465e04a3edd9ee7c36d2
YuTing-Fang1999 <66452317+YuTing-Fang1999@users.noreply.github.com>
2022-03-07 16:53:47 +0800

    Update uva11063 - B2-Sequence.cpp

--- one star/uva11063 - B2-Sequence.cpp
+++ one star/uva11063 - B2-Sequence.cpp
@@ -5,16 +5,22 @@ using namespace std;
 
 int N,T=0;
 int b[10005];
-map< int, bool > mark; //�]�i�H�ϥ�set�ӧP�_ 
 bool isB2;
 
+// 題目說B2不能有負數不代表輸入不會有負數!! 
+// 記得要空兩行!! 
 int main(){
 	while(cin>>N){
 		isB2=true;
-		mark.clear();
+		map< int, bool > mark;
 		
 		for(int i=0;i<N;++i) cin>>b[i];
+		
 		for(int i=0;i<N;++i) {
+			if(b[i]<=0 || (i>0 && b[i-1]>=b[i]) || !isB2){
+				isB2=false;
+				break;
+			}
 			for(int j=i;j<N;++j){
 				int sum = b[i]+b[j];
 				if(mark[sum]){
@@ -28,8 +34,8 @@ int main(){
 			
 		}
 		
-		if(isB2) printf("Case #%d: It is a B2-Sequence.\n",++T);
-		else printf("Case #%d: It is not a B2-Sequence.\n",++T);
+		if(isB2) printf("Case #%d: It is a B2-Sequence.\n\n",++T);
+		else printf("Case #%d: It is not a B2-Sequence.\n\n",++T);
 	}
 	return 0;
 }
