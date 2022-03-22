bbf047c29fc617e7db08e80f9d93f5b835e06d0a
YuTing-Fang1999 <66452317+YuTing-Fang1999@users.noreply.github.com>
2022-03-22 20:43:33 +0800

    Update p1.cpp

--- p1.cpp
+++ p1.cpp
@@ -6,14 +6,11 @@ int main(){
 	while(cin>>s && s){
 		long long page = 0;
 		long long tot = 0;
-		long long lose;
 		while(tot<=s){
 			++page;
 			tot += page;
 		}
-		if(s==tot) lose = page;
-		else lose = tot-s;
-		printf("%lld %lld\n",lose, page);
+		printf("%lld %lld\n",tot-s, page);
 	}
 	return 0;
 }
