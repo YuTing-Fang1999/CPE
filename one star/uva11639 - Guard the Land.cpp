6351a359843a5d45f2810c3caa1076d92f34c9b2
YuTing-Fang1999 <66452317+YuTing-Fang1999@users.noreply.github.com>
2022-06-06 17:44:59 +0800

    Update uva11639 - Guard the Land.cpp

--- one star/uva11639 - Guard the Land.cpp
+++ one star/uva11639 - Guard the Land.cpp
@@ -20,8 +20,7 @@ int main(){
 		int strong = 0;
 		if(strong_w>0 && strong_h>0) strong = strong_w * strong_h;
 		
-		int weak = (l1.x2-l1.x1) * (l1.y2-l1.y1) + (l2.x2-l2.x1) * (l2.y2-l2.y1);
-		weak -= (strong*2);
+		int weak = (l1.x2-l1.x1) * (l1.y2-l1.y1) + (l2.x2-l2.x1) * (l2.y2-l2.y1) - (strong*2);
 		printf("Night %d: %d %d %d\n", Night, strong, weak, 10000-strong-weak);
 	}
 	return 0;
